410f4719ad86862c3b568bb5ffee966a098a1dfd
valkubo <kubo@val.co.jp>
2024-11-14 21:44:39 +0900

    inputファイル配置

---
+++ convertToGTFS.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("処理開始")
+
+	fmt.Println("処理終了")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ValLaboratory/keio-railway
+
+go 1.19
+
+require golang.org/x/text v0.14.0
