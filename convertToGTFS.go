f40a66880075eb034591f3f126ab8400f742d84d
valkubo <kubo@val.co.jp>
2024-11-17 10:57:25 +0900

    routeMap連想配列の要素をroutes.txtに出力

--- convertToGTFS.go
+++ convertToGTFS.go
@@ -19,10 +19,21 @@ type Stop struct {
 	yomi string
 }
 
+// 系統
+type Route struct {
+	id   string
+	name string
+}
+
 // Stop連想配列
 // キー stop_id
 // 値 Stop
 var stopMap map[string]Stop = make(map[string]Stop)
+
+// Route連想配列
+// キー route_id
+// 値 Route
+var routeMap map[string]Route = make(map[string]Route)
 
 func main() {
 	fmt.Println("処理開始")
@@ -33,6 +44,11 @@ func main() {
 	writeStopsTxt()
 	// stopMap連想配列の要素をtranslations.txtに出力
 	writeTranslationsTxt()
+
+	// inpput/RouteMaster.tsvを読み込んで、routeをrouteMapに格納
+	readRouteMasterTsv()
+	// routeMap連想配列の要素をroutes.txtに出力
+	writeRoutesTxt()
 
 	fmt.Println("処理終了")
 }
@@ -64,6 +80,35 @@ func readStopMasterTsv() {
 		stop.name = elements[2]
 		stop.yomi = elements[3]
 		stopMap[stop.id] = stop
+	}
+}
+
+// inpput/RouteMaster.tsvを読み込んで、routeをrouteMap辞書に格納
+func readRouteMasterTsv() {
+	fmt.Println("RouteMaster.tsv読み込み")
+	var file string = "input/RouteMaster.tsv"
+	if _, err := os.Stat(file); err != nil {
+		fmt.Println("ファイルは存在しません！" + file)
+		os.Exit(1)
+	}
+	data, _ := os.Open(file)
+	defer data.Close()
+
+	var line string
+
+	scanner := bufio.NewScanner(data)
+	// 1行ずつ読み込み
+	for scanner.Scan() {
+		// 1行読み込み
+		line = sjis_to_utf8(scanner.Text())
+		// 1行をタブで分割
+		elements := strings.Split(line, "\t")
+		// route構造体を作成
+		var route Route = Route{}
+		// stop構造体に分割された要素を格納
+		route.id = elements[1]
+		route.name = elements[5]
+		routeMap[route.id] = route
 	}
 }
 
@@ -115,6 +160,31 @@ func writeTranslationsTxt() {
 			"stop_name",
 			"ja-Hrkt",
 			stop.yomi,
+		}
+		writer.Write(data)
+	}
+	writer.Flush()
+}
+
+// routeMap連想配列の要素をroutes.txtに出力
+func writeRoutesTxt() {
+	fmt.Println("routes.txt出力")
+	file, _ := os.Create("output/routes.txt")
+	defer file.Close()
+	var writer *csv.Writer = csv.NewWriter(transform.NewWriter(file, japanese.ShiftJIS.NewEncoder()))
+	writer.UseCRLF = true //改行コードを\r\nにする
+	// 見出し行を出力
+	data := []string{
+		"route_id",
+		"route_long_name",
+	}
+	writer.Write(data)
+	// routeMap連想配列の要素を取り出しながらループ
+	for _, route := range routeMap {
+		// routeをsroutes.txtに出力
+		data := []string{
+			route.id,
+			route.name,
 		}
 		writer.Write(data)
 	}
