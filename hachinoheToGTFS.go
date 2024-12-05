937c50ad590ee4f1504810c136cab4141877eeb5
valkubo <kubo@val.co.jp>
2024-12-05 16:49:28 +0900

    グローバル変数をtype.goに分割

--- hachinoheToGTFS.go
+++ hachinoheToGTFS.go
@@ -16,58 +16,6 @@ import (
 	"golang.org/x/text/transform"
 )
 
-// バス停
-type Stop struct {
-	id   string
-	name string
-	yomi string
-}
-
-// ポール
-type Pole struct {
-	id      string
-	stop_id string
-	name    string
-}
-
-// 系統
-type Route struct {
-	id   string
-	name string
-}
-
-// 便
-type Trip struct {
-	id        string
-	route_id  string
-	yobi      string
-	stopTimes []StopTime
-	col3      string // 3:通常  2:列車備考(MarkMaster)
-}
-
-// バス停と着発時刻
-type StopTime struct {
-	stop_id        string
-	arrival_time   string
-	departure_time string
-}
-
-// Stop連想配列
-// キー stop_id
-// 値 Stop
-var stopMap map[string]Stop = make(map[string]Stop)
-
-// Pole配列
-var poleList []Pole
-
-// Route連想配列
-// キー route_id
-// 値 Route
-var routeMap map[string]Route = make(map[string]Route)
-
-// Trip配列
-var tripList []Trip
-
 func main() {
 	fmt.Println("処理開始")
 
