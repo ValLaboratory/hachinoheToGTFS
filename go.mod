9465b07ec762a75ae25beb092009c6deaad582ec
valkubo <kubo@val.co.jp>
2024-12-20 17:17:44 +0900

    自動デリバリ 製品バージョンを見るよう修正  ファイルバージョンが0.0.0.0のままのため

--- go.mod
+++ go.mod
@@ -2,13 +2,13 @@ module github.com/ValLaboratory/hachinoheToGTFS
 
 go 1.19
 
-require golang.org/x/text v0.14.0
+require (
+	github.com/stretchr/testify v1.10.0
+	golang.org/x/text v0.14.0
+)
 
 require (
-	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/josephspurrier/goversioninfo v1.4.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- release.ps1
+++ release.ps1
@@ -7,10 +7,10 @@ $target = "hachinoheToGTFS.exe"
 # サーバーパス
 $path = "\\contents-storage\horus\tools\路線バス_ダイヤ取込\八戸市営_GTFSコンバーター\検証版\" + $target
 # ローカルファイルのファイルバージョン
-$v1 = (Get-ItemProperty $target).VersionInfo.FileVersion
+$v1 = (Get-ItemProperty $target).VersionInfo.ProductVersion
 if( Test-Path $path ){
   # サーバーファイルのファイルバージョン
-  $v2 = (Get-ItemProperty $path).VersionInfo.FileVersion
+  $v2 = (Get-ItemProperty $path).VersionInfo.ProductVersion
   if ( !( $v1 -eq $v2 ) ) {
     $result = [System.Windows.Forms.MessageBox]::Show("検証版にリリースしますか？",$v1,"YesNo","Question","Button2")
     if ($result -eq "Yes") {
